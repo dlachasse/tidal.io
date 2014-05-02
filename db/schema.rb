ab3ed7d0720c2eb215196d7becacceb88c3e856b
David La Chasse <david.lachasse@gmail.com>
2014-05-01 23:16:29 -0700

    Add password fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502053744) do
+ActiveRecord::Schema.define(version: 20140502061146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20140502053744) do
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
