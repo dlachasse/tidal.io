529e4467358b390a26c187fa7974ece5650b0238
David La Chasse <david.lachasse@gmail.com>
2014-05-03 14:49:04 -0700

    Update routes, remove unnecessary endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'signup', to: 'users#new', as: :signup
+  post 'signup', to: 'users#create'
+  resources :users, except: [:index, :new, :create] do
+	  member do
+	    get :activate
+	  end
+	end
+  root 'users#new'
 end
