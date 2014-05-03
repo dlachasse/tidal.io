b23b16ed2645fd09e2079c4f1c23521724f2bb6b
David La Chasse <david.lachasse@gmail.com>
2014-05-03 09:18:57 -0700

    Add tests for subscription

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Subscription do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it 'has a valid factory' do
+		expect(build(:subscription)).to be_valid
+	end
+
+	it { should belong_to :user }
+	it { should validate_presence_of :user_id }
+	it { should validate_presence_of :feed_id }
+
 end
