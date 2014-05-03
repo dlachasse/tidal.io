0135c4c3604f1fbf4cf63fc61fa94bd7a9662659
David La Chasse <david.lachasse@gmail.com>
2014-05-03 14:48:06 -0700

    Build out user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
+	respond_to :json, :html
+
+	def show
+		@user = User.find(params[:id])
+		render :json => @user.to_json(:only => [:id, :name, :email])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+      flash[:notice] ||= 'Account created.'
+    end
+    respond_with @user
+  end
+
+  def activate
+	  if (@user = User.load_from_activation_token(params[:id]))
+	    @user.activate!
+	    flash[:notice] ||= 'Your account was successfully activated.'
+	  else
+	    not_authenticated
+	  end
+	  respond_with @user, location: account_path(@user.id)
+	end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
