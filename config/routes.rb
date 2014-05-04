1be02625b123f1f0f3e5a7e84aa2f7e6eab6570e
David La Chasse <david.lachasse@gmail.com>
2014-05-04 12:56:17 -0700

    Add session resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
   get 'signup', to: 'users#new', as: :signup
   post 'signup', to: 'users#create'
   resources :users, except: [:index, :new, :create] do
-	  member do
-	    get :activate
-	  end
-	end
+    member do
+      get :activate
+    end
+  end
+
+  resources :sessions, except: [:index, :edit, :update]
+
   root 'users#new'
 end
