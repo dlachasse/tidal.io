82ee6e6974d3beb2469e2ff63def5ac9fd3eff9c
David La Chasse <david.lachasse@gmail.com>
2014-05-04 23:24:05 -0700

    Pull down feed data with feedjirra

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -7,4 +7,16 @@ class Feed < ActiveRecord::Base
 	has_many :users, through: :subscriptions, as: :subscribers
 	has_many :articles
 
+	# CALLBACKS
+	before_save :pull_down_feed
+
+	def pull_down_feed
+		@feed = Feedjira::Feed.fetch_and_parse(self.url)
+		assign_name
+	end
+
+	def assign_name
+		self.name = @feed.title
+	end
+
 end
