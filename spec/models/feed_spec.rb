6bcdf609dcbfb17d0a02822d690b6c1c7e90bed1
David La Chasse <david.lachasse@gmail.com>
2014-05-06 23:50:11 -0700

    Test Sidekiq queuing on saving new feed

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -8,6 +8,15 @@ describe Feed do
 	it { should have_many(:articles) }
 	it { should validate_presence_of(:url) }
 
+	context 'creating new feed' do
+
+		it 'creates a new enqueued job' do
+			new_feed = create(:feed)
+			expect(ArticleFetcherWorker).to have_enqueued_job(new_feed.url)
+		end
+
+	end
+
 	describe '#retrieve_name' do
 
 		it 'finds correct title from feed' do
