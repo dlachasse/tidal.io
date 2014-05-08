4571ea29e6c1951e022feed4c73b23503178ef7a
David La Chasse <david.lachasse@gmail.com>
2014-05-07 23:05:49 -0700

    Test for permalink validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,9 @@ describe Article do
 	it { should belong_to(:feed) }
 	it { should validate_presence_of(:feed_id) }
 
+	it 'properly populates permalink when missing domain' do # alliterations!
+		sweet_blog = create(:feed, url: 'http://blog.sensible.io/rss')
+		FeedParser.retrieve_entries(sweet_blog.url)
+		expect(sweet_blog.articles.pluck(:permalink)).to include('http://blog.sensible.io/rss/2013/08/19/postgresql-basics-by-example.html')
+	end
 end
