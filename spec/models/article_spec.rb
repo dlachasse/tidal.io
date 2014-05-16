2f035644b41bdce50a8ccdd0864b04daaeb95efe
David La Chasse <david.lachasse@gmail.com>
2014-05-15 20:56:34 -0700

    Fix article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,6 +12,6 @@ describe Article do
 	it 'properly populates permalink when missing domain' do # alliterations!
 		sweet_blog = create(:feed, url: 'http://blog.sensible.io/rss')
 		FeedParser.retrieve_entries(sweet_blog.url)
-		expect(sweet_blog.articles.pluck(:permalink)).to include('http://blog.sensible.io/rss/2013/08/19/postgresql-basics-by-example.html')
+		expect(sweet_blog.articles.pluck(:permalink)).to include('http://blog.sensible.io/rss/2014/03/07/getting-started-with-ember-app-kit.html')
 	end
 end
