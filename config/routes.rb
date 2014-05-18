efe9dfadf7495992db903ff6869dd811cdcf6132
David La Chasse <david.lachasse@gmail.com>
2014-05-18 16:33:28 -0700

    Scope all routes to /api endpoints
    This will mitigate name collisions with
    JS routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
 
-  get 'signup', to: 'users#new', as: :signup
-  post 'signup', to: 'users#create'
-  resources :users, except: [:index, :new, :create] do
-    member do
-      get :activate
+  scope 'api' do
+    resources :users, except: [:index] do
+      member do
+        get :activate
+      end
+      get 'feeds', to: 'subscriptions#index'
     end
-    get 'feeds', to: 'subscriptions#index'
+
+    get 'login', to: 'sessions#new', as: :login
+    resources :sessions, except: [:index, :edit, :update, :new]
+
+    resources :feeds, except: [:index, :edit, :update] do
+      get 'articles', to: 'articles#index'
+    end
+
+    get 'article/:id', to: 'articles#show'
   end
-
-  get 'login', to: 'sessions#new', as: :login
-  resources :sessions, except: [:index, :edit, :update, :new]
-
-  resources :feeds, except: [:index, :edit, :update] do
-    get 'articles', to: 'articles#index'
-  end
-
-  get 'article/:id', to: 'articles#show'
 
   mount Sidekiq::Web => '/sidekiq'
 
