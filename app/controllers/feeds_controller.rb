96fd247c7a459d2a5903c8bba5349d399c6d065b
David La Chasse <david.lachasse@gmail.com>
2014-05-28 22:30:24 -0700

    Update Feeds controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,6 @@
 class FeedsController < ApplicationController
   after_action :subscribe_user, only: [:create]
+  before_action :grab_user, only: [:create]
   before_action :validate_feed, only: [:create]
   respond_to :json, :html
 
@@ -13,10 +14,10 @@ class FeedsController < ApplicationController
   end
 
   def create
-    if @rss.is_a? Array
+    if @rss.is_a?(Array) && @rss.count > 1
       render :json => @rss.to_json
     else
-  	  @feed = Feed.where(url: @rss).first_or_create(url: @rss)
+  	  @feed = Feed.where(url: @rss.first).first_or_create(url: @rss.first)
       location = @feed.save ? feed_path(@feed) : new_feed_path
       respond_with @feed
     end
@@ -33,13 +34,16 @@ class FeedsController < ApplicationController
   end
 
   def subscribe_user
-    if @feed
-      Subscription.create!(feed_id: @feed.id, user_id: session[:user_id])
-    end
+    Subscription.create_subscription(@user.id, @feed.id) unless @feed.nil?
   end
 
   def validate_feed
-    @rss = FeedParser.discover_rss(params[:feed][:url])
+    @rss = []
+    FeedParser.discover_rss(params[:feed][:url]).map { |url| @rss << url }
+  end
+
+  def grab_user
+    @user = User.find(params[:user_id])
   end
 
 end
