7ed0428e914aa14b9d5bc3f732eb4e1f44431508
David La Chasse <david.lachasse@gmail.com>
2014-05-28 22:29:43 -0700

    Fix finder method

--- app/classes/feed_parser.rb
+++ app/classes/feed_parser.rb
@@ -24,7 +24,7 @@ class FeedParser
 	end
 
 	def self.check_and_save_article article
-		Article.where(permalink: article_url).first_or_create!(
+		Article.where(permalink: article.url).first_or_create!(
 			title: article.title,
 			permalink: article.url,
 			body: article.content || article.summary,
