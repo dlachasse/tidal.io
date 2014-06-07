cf25906f6a0f470ed14759c6aeae1b76590c2246
Swashcap <swashcap@gmail.com>
2014-06-07 15:29:20 -0700

    Begin building out Feed and Article services.

---
+++ app/assets/javascripts/services/article.js
@@ -0,0 +1,13 @@
+angular.module('tidal')
+  .factory('article', ['$http', function ($http) {
+    var API_URL = '/api/article';
+
+    return {
+      get: function (id) {
+        return $http(API_URL + '/' + id);
+      },
+      update: function (id, data) {
+        return $http.put(API_URL + '/' + id, data);
+      }
+    };
+  }]);
---
+++ app/assets/javascripts/services/feed.js
@@ -0,0 +1,21 @@
+angular.module('tidal')
+  .factory('feed', ['$http', function ($http) {
+    var API_URL = '/api/feeds';
+
+    return {
+      create: function (data) {
+        return $http.post(API_URL, data)
+      },
+      get: function (id) {
+        if (id != null) {
+          return $http.get(API_URL + '/id');
+        }
+      },
+      update: function (id, data) {
+        return $http.put(API_URL + '/id', data);
+      },
+      delete: function (id) {
+        return $http.delete(API_URL + '/id');
+      }
+    };
+  }]);
