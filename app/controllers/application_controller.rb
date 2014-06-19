bb8ad4006963b98830b97b6b51b411e5fcdc1e76
David La Chasse <david.lachasse@gmail.com>
2014-06-18 22:44:37 -0700

    Turn off authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/tidal.v1' }
-  before_filter :restrict_access
+  # before_filter :restrict_access
 
   def restrict_access
 	  authenticate_or_request_with_http_token do |token, options|
