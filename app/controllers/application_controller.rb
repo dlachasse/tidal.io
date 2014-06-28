af5d7632f91d394099f618bfea18823ae4981075
David La Chasse <david.lachasse@gmail.com>
2014-06-28 11:33:13 -0700

    Add auth headers if user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/tidal.v1' }
   before_filter :restrict_access
+  before_filter :set_headers # use this only for testing, adds additional query
 
   def restrict_access
 	  authenticate_or_request_with_http_token do |token, options|
 	    User.exists?(api_key: token)
+	  end
+	end
+
+	def set_headers
+		if current_user
+	    request.headers['Accept'] = 'application/tidal.v1'
+	    request.headers['Authorization'] = "Token token=#{current_user.api_key}"
 	  end
 	end
 end
