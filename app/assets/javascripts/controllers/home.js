de9a5853fb535d64cb5ad93fd1c852fd3da19152
Cory Reed <cory@electricpulp.com>
2014-08-28 20:49:31 -0700

    Implement 'Load more' feature on the home view.

--- app/assets/javascripts/controllers/home.js
+++ app/assets/javascripts/controllers/home.js
@@ -4,14 +4,12 @@ angular.module('tidal')
   .controller('HomeCtrl', ['$scope', 'user', '$http', function ($scope, user, $http) {
     var ARTICLE_COUNT = 10;
 
-    $scope.user = user;
-    $scope.start = 0;
-
     var getArticlesUrl = function (count, start) {
       if ('id' in user) {
         return 'api/users/' + user.id + '/feeds';
       }
     };
+
     var getArticlesConfig = function (start) {
       // Set default for `start`
       if (typeof start === 'undefined') {
@@ -26,9 +24,21 @@ angular.module('tidal')
       }
     };
 
+    $scope.user = user;
+    $scope.start = 0;
+
+    $scope.loadArticles = function (start) {
+      $http.get(getArticlesUrl(), getArticlesConfig(start))
+        .then(function (data) {
+          $scope.articles = $scope.articles.concat(data.data);
+          $scope.start = $scope.articles.length;
+        });
+    };
+
     // Get initial load of articles
     $http.get(getArticlesUrl(), getArticlesConfig())
       .then(function (data) {
         $scope.articles = data.data;
+        $scope.start = $scope.articles.length;
       });
   }]);
