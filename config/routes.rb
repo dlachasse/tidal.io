e883577cb4899eb411bb0ce12fc9e5323059b9e4
David La Chasse <david.lachasse@gmail.com>
2014-09-09 19:16:58 -0700

    Remove whitespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
 
-
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, except: [:index] do
