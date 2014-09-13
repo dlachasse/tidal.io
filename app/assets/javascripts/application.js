e26b0caa7f1a79e233bba0ba86964236f4de8d89
David La Chasse <david.lachasse@gmail.com>
2014-09-13 09:25:59 -0700

    Reorganize js manifest
    When storing entire vendor folders in
    assets directory, all contents are compressed
    and added to public/assets folder upon
    compilation. We only want source files to be
    compressed. Also, include template generator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,17 +10,14 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require vendor/fastclick/lib/fastclick.js
-//= require vendor/angular/angular.min.js
-//= require vendor/angular-resource/angular-resource.min.js
-//= require vendor/angular-route/angular-route.min.js
-//= require vendor/angular-sanitize/angular-sanitize.min.js
-//= require vendor/angular-ui-router/release/angular-ui-router.min.js
+//= require vendor/fastclick.js
+//= require vendor/angular.min.js
+//= require vendor/angular-resource.min.js
+//= require vendor/angular-route.min.js
+//= require vendor/angular-sanitize.min.js
+//= require vendor/angular-ui-router.min.js
 //
-// Get all the Angular templates
-// (https://github.com/pitr/angular-rails-templates)
-//= require angular-rails-templates
-//= require_tree ../templates
+//= require ../templates/templates-complete.js.erb
 //
 //= require bootstrap.js
 //= require_tree ./constants
