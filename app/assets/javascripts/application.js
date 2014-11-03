eb28be8d25503a4dad20b7238f007b3c93613867
David La Chasse <david.lachasse@gmail.com>
2014-11-02 16:07:41 -0800

    Remove references to JS vendor folders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require vendor/fastclick/lib/fastclick.js
-//= require vendor/angular/angular.min.js
-//= require vendor/angular-resource/angular-resource.min.js
-//= require vendor/angular-route/angular-route.min.js
-//= require vendor/angular-sanitize/angular-sanitize.min.js
-//= require vendor/angular-ui-router/release/angular-ui-router.min.js
+//= require vendor/fastclick.js
+//= require vendor/angular.min.js
+//= require vendor/angular-resource.min.js
+//= require vendor/angular-route.min.js
+//= require vendor/angular-sanitize.min.js
+//= require vendor/angular-ui-router.min.js
 //
 // Get all the Angular templates
 // (https://github.com/pitr/angular-rails-templates)
